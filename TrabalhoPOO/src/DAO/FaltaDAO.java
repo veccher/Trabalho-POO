73c8e1780d432740721769a7b2d9e3d55c57d511
Vitor <Vitor@192.168.1.8>
2015-05-07 00:05:40 -0300

    Modificações na Classe FaltaDAO para criação da lista, exclusao/busca de algum elemento

---
+++ TrabalhoPOO/src/DAO/FaltaDAO.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DAO;
+
+import Pojo.Aluno;
+import Pojo.Falta;
+import Pojo.Turma;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ *
+ * @author Vitor
+ */
+public class FaltaDAO {
+    List<Falta> listaFalta = new ArrayList<Falta>();
+    
+    public void adicionar(Falta falta){
+        listaFalta.add(falta);
+        Collections.sort(listaFalta);
+    }
+    
+    public boolean excluiFalta(Aluno aluno, Turma turma){
+        
+        Falta aux = new Falta(new Integer(0),aluno,turma);
+        return this.listaFalta.remove(aux);
+        
+   }
+    
+    public Falta buscaFalta(Aluno aluno, Turma turma){
+        Falta aux = new Falta(new Integer(0),aluno,turma);
+        
+        for(Falta falta : listaFalta){
+            if(falta.equals(aux)){
+                return falta;
+            }
+            
+        }
+        return null;
+    }
+    
+    
+    
+}
